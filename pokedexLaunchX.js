cc1ce736263a75e0ad3484c58bd2d6e655c79c1d
edsonhr <edson.her.ram@gmail.com>
2022-03-26 13:56:35 -0600

    Función para mostrar tipo, estadisticas y nombre de pokemon

--- pokedexLaunchX.js
+++ pokedexLaunchX.js
@@ -24,22 +24,51 @@ const fetchPokemon = () => {
         let pokeEstadisticas = data.stats;
         let pokeMovimientos = data.moves;
 
-        console.log(pokeImg);
         console.log(pokeNombre);
+        console.log(pokeImg);
         console.log(pokeTipo);
-        //Pruebas para imprimir estadisticas por separado
-        for (var i=0; i <3; i++)
-        {
-            console.log(pokeEstadisticas[i].base_stat);
-        }
         //console.log(pokeEstadisticas);
-        console.log(pokeMovimientos);
+        //console.log(pokeMovimientos);
+        pokeNombrePokemon(pokeNombre);
         pokeImage(pokeImg);
+        pokeTipoPokemon(pokeTipo);
+        pokeEstadisticasPokemon(pokeEstadisticas);
     })
 }
 
-//Funcion para obtener la imagen del pokemon
+//Funcion para obtener el nombre del pokemon
+const pokeNombrePokemon = (pokeNombre) => {
+    document.getElementById("pokeNombre").innerHTML= pokeNombre;
+}
+
+//Funcion obtener la imagen del pokemon
 const pokeImage = (url) => {
     const pokeImg = document.getElementById("pokeImg");
     pokeImg.src =  url;
+}
+
+//Funcion para obtener el tipo de pokemon
+const pokeTipoPokemon = (pokeTipo) => {
+    document.getElementById("pokeTipo").innerHTML="Tipo: " + pokeTipo;
+}
+
+//Funcion para obtener estadisticas del pokemon
+const pokeEstadisticasPokemon = (pokeEstadisticas) => {
+    var new_div = document.createElement("div");
+    var espacio = document.createElement("br");
+
+    let tam = Object.keys(pokeEstadisticas).length; //Obtengo el tamaño del objeto
+        for (var i=0; i < tam; i++)
+        {
+            var content = document.createTextNode("Esfuerzo: " + pokeEstadisticas[i].effort);       
+            new_div.appendChild(content);                                     
+            document.getElementById("estadisticas").appendChild(new_div+espacio);
+            
+            var content = document.createTextNode("Nombre: " + pokeEstadisticas[i].stat.name);       
+            new_div.appendChild(content);                                         
+            document.getElementById("estadisticas").appendChild(new_div);
+
+            console.log("Esfuerzo: " + pokeEstadisticas[i].effort);
+            console.log("Nombre: " + pokeEstadisticas[i].stat.name);
+        }
 }
